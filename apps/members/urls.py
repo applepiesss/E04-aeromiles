d65483ab57e0779b7f5634a73753517442ada5f2
firstaqila <saffana.firsta@gmail.com>
2026-04-30 18:59:22 +0700

    feat: Implement Manajemen Data Member

--- apps/members/urls.py
+++ apps/members/urls.py
@@ -1,5 +1,11 @@
+from django.urls import path
+from .views import kelola_member, tambah_member, edit_member, hapus_member
+
 app_name = 'members'
 
 urlpatterns = [
-
+    path('kelola/', kelola_member, name='kelola_member'),
+    path('kelola/tambah/', tambah_member, name='tambah_member'),
+    path('kelola/edit/<str:nomor>/', edit_member, name='edit_member'),
+    path('kelola/hapus/<str:nomor>/', hapus_member, name='hapus_member'),
 ]
