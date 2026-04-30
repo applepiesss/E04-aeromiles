b8de92117fb740c29a0dad9a86932d2feb781edf
applepiesss <nadiaaiiisyahf@gmail.com>
2026-04-30 21:30:24 +0700

    Completed logout and register

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import show_main, login_view, dashboard
+from .views import logout_view, register_view, show_main, login_view, dashboard
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
     path('dashboard/', dashboard, name='dashboard'),
 ]
