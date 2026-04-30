8d5e8f89e74931b41dfaf52ccb80696f2abe81bc
Peter <123148242+petery05@users.noreply.github.com>
2026-04-30 23:56:06 +0700

    Update session handling in views for staff role

--- apps/vendors/views.py
+++ apps/vendors/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def daftar_hadiah(request):
-    return render(request, 'coba.html')
+    request.session['role'] = 'staff'
+
+    request.session['username'] = 'Staf Admin'
+    return render(request, 'manajemen_hadiah_penyedia.html')
+
+def manage_mitra_view(request):
+    request.session['role'] = 'staff'
+    return render(request, 'manajemen_mitra.html')
