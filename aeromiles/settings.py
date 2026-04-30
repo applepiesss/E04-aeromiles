d400453ab227ac45d02f16f551670bd403c9dde7
HODEMASTER <peteryap0505@gmail.com>
2026-04-30 12:44:50 +0700

    refactor: making new folder for apps and making vendor apps

--- aeromiles/settings.py
+++ aeromiles/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    'apps.main',
+    'apps.vendors'
 ]
 
 MIDDLEWARE = [
