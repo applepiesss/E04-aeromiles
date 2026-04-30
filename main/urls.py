0f0f054fbacdf722b9c443b965912f45b0b1015a
Rekkin99 <farrellbagoes04@gmail.com>
2026-04-30 14:08:18 +0700

    Add Profile Settings Page

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, profile_settings, change_password
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('profile-settings/', profile_settings, name='profile_settings'),
+    path('change-password/', change_password, name='change_password'),
 ]
