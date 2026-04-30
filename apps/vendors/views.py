e69cc02ac01a4cb7b80c0e49fbadc4947420f118
Peter <123148242+petery05@users.noreply.github.com>
2026-04-30 23:54:55 +0700

    Update apps/vendors/views.py

--- apps/vendors/views.py
+++ apps/vendors/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def daftar_hadiah(request):
-    return render(request, 'katalog_hadiah.html')
+    return render(request, 'coba.html')
