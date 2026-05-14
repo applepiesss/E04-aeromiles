8ef0084778dea9b85ff06dfca8f9cca6925947ff
HODEMASTER <peteryap0505@gmail.com>
2026-05-14 20:46:44 +0700

    implement delete mitra

--- apps/vendors/urls.py
+++ apps/vendors/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apps.vendors.views import daftar_hadiah, manage_mitra_view, save_mitra
+from apps.vendors.views import daftar_hadiah, manage_mitra_view, save_mitra, delete_mitra
 
 app_name = 'vendors'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('katalog_hadiah/', daftar_hadiah, name='daftar_hadiah'),
     path('manajemen-mitra/', manage_mitra_view, name='manage_mitra'),
     path('manajemen-mitra/save/', save_mitra, name='save_mitra'),
+    path('manajemen-mitra/delete/', delete_mitra, name='delete_mitra'),
 ]
