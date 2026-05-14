615f024be7ccaa29f1f49eaa3c16faff594a03bc
HODEMASTER <peteryap0505@gmail.com>
2026-05-14 19:30:20 +0700

    implement read mitra

--- apps/vendors/views.py
+++ apps/vendors/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection
+from django.contrib import messages
+import datetime
+
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
 # Create your views here.
 
@@ -9,4 +16,16 @@ def daftar_hadiah(request):
     return render(request, 'manajemen_hadiah_penyedia.html')
 
 def manage_mitra_view(request):
-    return render(request, 'manajemen_mitra.html')
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT email_mitra, id_penyedia, nama_mitra, tanggal_kerja_sama
+            FROM MITRA
+            ORDER BY tanggal_kerja_sama DESC
+        """)
+        partners = dictfetchall(cursor)
+    for p in partners:
+        if p['tanggal_kerja_sama']:
+            p['tanggal_kerja_sama_fmt'] = p['tanggal_kerja_sama'].strftime('%d %B %Y')
+            p['tanggal_kerja_sama_iso'] = p['tanggal_kerja_sama'].strftime('%Y-%m-%d')
+            
+    return render(request, 'manajemen_mitra.html', {'partners': partners})
